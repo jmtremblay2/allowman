812182203c4350f3f584878d4b2c70748621c019
Jean-Michel Tremblay <jeanmi.tremblay@gmail.com>
2019-06-16 21:06:12 -0400

    code to connect to the postgres database

---
+++ connect.go
@@ -0,0 +1,41 @@
+
+package main
+
+import (
+  "database/sql"
+  "fmt"
+
+  _ "github.com/lib/pq"
+)
+
+const (
+  host     = "localhost"
+  port     = 5432
+  user = "postgres"
+  password = "postgres"
+  dbname   = "allowman"
+)
+
+func main() {
+  psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+    "password=%s dbname=%s sslmode=disable",
+    host, port, user, password, dbname)
+
+  db, err := sql.Open("postgres", psqlInfo)
+  if err != nil {
+    panic(err)
+  }
+  defer db.Close()
+
+  err = db.Ping()
+  if err != nil {
+    panic(err)
+  }
+
+  insertStmt := `INSERT INTO households(hid) VALUES ($1)`
+  _, err = db.Exec(insertStmt, 2)
+  if err != nil {
+    panic(err)
+  }
+  fmt.Println("Successfully connected!")
+}
